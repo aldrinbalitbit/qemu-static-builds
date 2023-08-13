8aa9991c8dd538c2b8223849e04c593bf6b608f9
aldrinbalitbit <aldrinbalitbit1127@gmail.com>
2023-08-13 15:08:51 +0800

    List of files

--- build.sh
+++ build.sh
@@ -11,17 +11,19 @@ git clone https://github.com/qemu/qemu.git >> "${_cur_dir}"/qemu-build/build.log
 cd qemu
 _msg "Updating submodules"
 git submodule update --init --recursive >> "${_cur_dir}"/qemu-build/build.log 2>&1
-_msg "Configuring qemu"
+_msg "Configuring static for qemu"
 ./configure --prefix="${_cur_dir}"/qemu-build \
 	    --static                          \
 	    --enable-gcrypt                   \
 	    --disable-system                  \
 	    --disable-werror >> "${_cur_dir}"/qemu-build/build.log 2>&1
-_msg "Building qemu"
+_msg "Building static forqemu"
 make -j16 >> "${_cur_dir}"/qemu-build/build.log
-_msg "Installing qemu"
+_msg "Installing qemu static binaries"
 make install >> "${_cur_dir}"/qemu-build/build.log
 _msg "Stripping qemu static binaries"
 find "${_cur_dir}"/qemu-build/bin -type f -exec strip --strip-all {} ';' >> "${_cur_dir}"/qemu-build/build.log
 _msg "Copying qemu static binaries to the new build directory"
-cp "${_cur_dir}"/qemu-build/{build.log,bin/qemu-*-static} "${_cur_dir}"/build
+ls "${_cur_dir}"/qemu-build/*
+ls "${_cur_dir}"/qemu-build/*/*
+cp "${_cur_dir}"/qemu-build/build.log "${_cur_dir}"/build
